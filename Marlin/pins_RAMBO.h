dbf917d5f6dfd5433db67a6116b1382dfcf6ee86
pizzyflavin <pizzyflavin@gmail.com>
2017-12-05 20:51:19 -0500

    Redefine HEATER_BED_PIN to unused pin so temp ISR doesn't keep turning it off

--- Marlin/pins_RAMBO.h
+++ Marlin/pins_RAMBO.h
@@ -161,7 +161,7 @@
 #define HEATER_0_PIN        9
 #define HEATER_1_PIN        7
 #define HEATER_2_PIN        6
-#define HEATER_BED_PIN      3
+#define HEATER_BED_PIN      46
 
 #define FAN_PIN             8
 #define FAN1_PIN            6
